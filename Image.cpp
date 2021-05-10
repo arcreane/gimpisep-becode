7cdc85153dcf80e901b80307b8b10c53c33fbc07
Lwii-U <louis.develter@gmail.com>
2021-05-10 15:57:44 +0200

    Implemented Image methods

--- Image.cpp
+++ Image.cpp
@@ -1,1 +1,33 @@
+#include <string>
 #include "Image.h"
+
+using namespace std;
+
+Image::Image() {
+	name = "";
+	path = "";
+}
+
+Image::Image(string name, string path) {
+	this->name = name;
+	this->path = path;
+}
+
+Image::~Image() {
+}
+
+string Image::getName() {
+	return name;
+}
+
+void Image::setName(string name) {
+	this->name = name;
+}
+
+string Image::getPath() {
+	return path;
+}
+
+void Image::setPath(string path) {
+	this->path = path;
+}
