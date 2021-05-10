812e5ebcccd15f544a852e874a44d334229e8683
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-10 14:29:12 +0200

    Hello world open cv image

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
+using namespace cv;
 using namespace std;
 
 int main() {
 	cout << "Hello world" << endl;
+
+	Mat image;
+
+	// Read source image
+	image = imread("./resources/van_gogh.jpg");
+
+	if (image.empty())
+	{
+		cout << "No image!\n" << endl;
+		return -1;
+	}
+
+
+	imshow("Image", image);
+
+	waitKey(0);
+
 	return 0;
 }
