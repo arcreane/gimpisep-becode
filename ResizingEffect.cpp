0dd9a53bba14c25c38ff85d1262387a9828a0be9
Lwii-U <louis.develter@gmail.com>
2021-05-16 19:23:28 +0200

    Implement resizing effect (finished)

---
+++ ResizingEffect.cpp
@@ -0,0 +1,40 @@
+#include <opencv2/opencv.hpp>
+#include "ResizingEffect.h"
+
+using namespace cv;
+
+ResizingEffect::ResizingEffect() {
+	this->scaleX = 1;
+	this->scaleY = 1;
+}
+
+ResizingEffect::ResizingEffect(double scaleX, double scaleY) {
+	this->scaleX = scaleX;
+	this->scaleY = scaleY;
+}
+
+ResizingEffect::~ResizingEffect() {
+
+}
+
+
+double ResizingEffect::getScaleX() {
+	return scaleX;
+}
+
+void ResizingEffect::setScaleX(double scaleX) {
+	this->scaleX = scaleX;
+}
+
+double ResizingEffect::getScaleY() {
+	return scaleY;
+}
+
+void ResizingEffect::setScaleY(double scaleY) {
+	this->scaleY = scaleY;
+}
+
+
+void ResizingEffect::applyEffect(Mat& source, Mat& result) {
+	resize(source, result, Size(), this->scaleX, this->scaleY, INTER_LINEAR);
+}
--- ResizingEffect.h
+++ ResizingEffect.h
@@ -1,8 +1,25 @@
 #pragma once
 #include "Effect.h"
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
 class ResizingEffect :
     public Effect
 {
+private:
+    double scaleX;
+    double scaleY;
+public:
+    ResizingEffect();
+    ResizingEffect(double scaleX, double scaleY);
+    ~ResizingEffect();
 
+    double getScaleX();
+    void setScaleX(double scaleX);
+    double getScaleY();
+    void setScaleY(double scaleY);
+
+    void applyEffect(Mat& source, Mat& result);
 };
 
