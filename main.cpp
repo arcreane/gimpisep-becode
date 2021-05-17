271e3a6d81d4363888a8618ba0f96428b6ee209d
GaspardCotterot <57559923+GaspardCotterot@users.noreply.github.com>
2021-05-17 16:41:49 +0200

    effet canny

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include "LightenDarken.h"
-#include "Erosion.h"
-#include "Dilation.h"
-#include "ResizingEffect.h"
+#include "CannyEdge.h"
 
 using namespace cv;
 using namespace std;
@@ -11,10 +8,10 @@ using namespace std;
 int main() {
 	cout << "Hello world" << endl;
 
-	Mat image, dilationImage, erosionImage;
+	Mat image;
 
 	// Read source image
-	image = imread("./resources/van_gogh.jpg");
+	image = imread("./resources/françois.jpg");
 
 	if (image.empty())
 	{
@@ -22,45 +19,21 @@ int main() {
 		return -1;
 	}
 
-	image.copyTo(dilationImage);
-	image.copyTo(erosionImage);
-	 
-	Dilation dilation(1,2);
-	Erosion erosion(1,2);
 
-	dilation.applyEffect(image, dilationImage);
-	erosion.applyEffect(image, erosionImage);
+	Mat canny;
+	CannyEdge effetCanny;
+	effetCanny.setBlurEffect(3);
+	effetCanny.setHighThreshold(100);
+	effetCanny.setLowThreshold(50);
+	effetCanny.setKermelSize(3);
 
-	//DilatationAndErosion effect;
-	//copy = effect.applyEffect(copy);
-	imshow("Image", image);
-	imshow("Result dilation", dilationImage);
-	imshow("Result erosion", erosionImage);
+	effetCanny.applyEffect(image, canny);
+	imshow("canny image", canny);
 
-
-	//gaspard
-	Mat imageResult;
-	LightenDarken effect;
-	effect.setBrightness(100);
-	effect.applyEffect(image, imageResult);
-	imshow("Image bright", imageResult);
-
-	//louis
-	Mat resized;
-	ResizingEffect effectLouis = ResizingEffect(0.5, 0.5);
-	effectLouis.applyEffect(image, resized);
-
-	imshow("Image", image);
-	imshow("Resized image", resized);
 
 	
 
 	waitKey(0);
-
-	//String test;
-	//cin >> test;
-	//cout << "Test + " + test << endl;
-	
 
 	return 0;
 }
