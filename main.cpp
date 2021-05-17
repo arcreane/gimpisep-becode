e98aabf2d7403276b0d05c015e1f5298cad15955
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-17 15:38:54 +0200

    first ui

--- main.cpp
+++ main.cpp
@@ -8,10 +8,60 @@
 using namespace cv;
 using namespace std;
 
+void UiCallBackFunc(int event, int x, int y, int flags, void* param)
+{
+	Mat& image = *(Mat*)param;
+	if (event == EVENT_LBUTTONDBLCLK)
+	{
+		cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+		if (x < 200) {
+			cout << "Erosion" << endl;
+			Erosion erosion(1, 2);
+			erosion.applyEffect(image, image);
+		}
+		else if (x > 200 && x < 400) {
+			cout << "Dilation" << endl;
+			Dilation dilation(1,2);
+			dilation.applyEffect(image, image);
+		}
+
+		if (x > 400) cout << "Resize" << endl;
+
+	}
+	/*else if (event == EVENT_RBUTTONDBLCLK)
+	{
+		cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+	else if (event == EVENT_MBUTTONDBLCLK)
+	{
+		cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+	
+	else if (event == EVENT_MOUSEMOVE)
+	{
+		cout << "Mouse move over the window - position (" << x << ", " << y << ")" << endl;
+
+	}*/
+}
+
+
 int main() {
+	static String imageName = "Image";
+	static String uiName = "Interface utilisateur";
+
 	cout << "Hello world" << endl;
 
-	Mat image, dilationImage, erosionImage;
+	static Mat image;
+	Mat ui(Size(600, 200), CV_8UC3);
+
+	//add icons to the UI
+	Mat icon1 = imread("./resources/noun_Avalanche.png");
+	Mat icon2 = imread("./resources/noun_Eye.png");
+	Mat icon3 = imread("./resources/noun_Resize.png");
+	icon1.copyTo(ui(Rect(0, 0, 200, 200)));
+	icon2.copyTo(ui(Rect(200, 0, 200, 200)));
+	icon3.copyTo(ui(Rect(400, 0, 200, 200)));
+
 
 	// Read source image
 	image = imread("./resources/van_gogh.jpg");
@@ -22,45 +72,24 @@ int main() {
 		return -1;
 	}
 
-	image.copyTo(dilationImage);
-	image.copyTo(erosionImage);
-	 
-	Dilation dilation(1,2);
-	Erosion erosion(1,2);
+	//Images
+	namedWindow(imageName);
+	namedWindow(uiName);
+	/*
+	imshow(imageName, image);
+	imshow("Result dilation", result);
+	imshow(uiName, ui);*/
+	setMouseCallback(uiName, UiCallBackFunc, (void*)&image);
 
-	dilation.applyEffect(image, dilationImage);
-	erosion.applyEffect(image, erosionImage);
+	for (;;) {
+		imshow(imageName, image);
+		imshow(uiName, ui);
+		if (cv::waitKey(15) == 27) break;
+	}
 
-	//DilatationAndErosion effect;
-	//copy = effect.applyEffect(copy);
-	imshow("Image", image);
-	imshow("Result dilation", dilationImage);
-	imshow("Result erosion", erosionImage);
-
-
-	//gaspard
-	Mat imageResult;
-	LightenDarken effect;
-	effect.setBrightness(100);
-	effect.applyEffect(image, imageResult);
-	imshow("Image bright", imageResult);
-
-	//louis
-	Mat resized;
-	ResizingEffect effectLouis = ResizingEffect(0.5, 0.5);
-	effectLouis.applyEffect(image, resized);
-
-	imshow("Image", image);
-	imshow("Resized image", resized);
 
 	
-
-	waitKey(0);
-
-	//String test;
-	//cin >> test;
-	//cout << "Test + " + test << endl;
-	
-
 	return 0;
 }
+
+
